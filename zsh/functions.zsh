29ccda8f9e5ff2e9fcb03f396ebc8a92759c2623
Jayakorn Karikan <jayakornk@gmail.com>
2024-08-18 11:43:54 +0700

    added navigation and reload functions

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -1,3 +1,19 @@
+# navigation
+cx() { cd "$@" && l; }
+fcd() { cd "$(find . -type d -not -path '*/.*' | fzf)" && l; }
+f() { echo "$(find . -type f -not -path '*/.*' | fzf)" | pbcopy }
+fv() { nvim "$(find . -type f -not -path '*/.*' | fzf)" }
+
+function reload {
+  # Delete current completion cache
+  command rm -f $_comp_dumpfile $ZSH_COMPDUMP
+
+  # Old zsh versions don't have ZSH_ARGZERO
+  local zsh="${ZSH_ARGZERO:-${functrace[-1]%:*}}"
+  # Check whether to run a login shell
+  [[ "$zsh" = -* || -o login ]] && exec -l "${zsh#-}" || exec "$zsh"
+}
+
 # take functions
 
 # mkcd is equivalent to takedir
